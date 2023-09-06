423661ee860fd166b6b8997f6c7ad0086d9eab23
Excellencer <kalle.liljestrom98@gmail.com>
2023-09-06 17:54:35 +0300

    Initial WebWeek1 commit.

---
+++ src/index.js
@@ -0,0 +1,28 @@
+
+
+/* From course video JavaScript 1 */
+if (document.readyState !== "loading") {
+  initializeCode();
+} else {
+  document.addEventListener("DOMContentLoaded", function () {
+    initializeCode();
+  });
+}
+
+function initializeCode() {
+  document.getElementById("my-button").addEventListener("click", myNotebook);
+  document.getElementById("add-data").addEventListener("click", customText);
+}
+
+function myNotebook() {
+  console.log("hello world");
+  document.getElementById("change").innerHTML = "Moi maailma";
+}
+
+function customText() {
+  let areaText = document.getElementById("custom-text").value;
+  let ul = document.getElementById("un-list");
+  let li = document.createElement("li");
+  li.appendChild(document.createTextNode(areaText));
+  ul.appendChild(li);
+}
